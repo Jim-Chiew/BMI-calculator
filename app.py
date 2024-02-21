c92820deaa204690fe9402558ee89384b55fa511
Jim <jimchiew112233@gmail.com>
2024-02-21 09:41:59 +0800

    Updated integration of age calculator, status and suggestions.

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, make_response
 import pickle
 from pandas import DataFrame
 import os.path
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 # ______________ Database_________________
@@ -26,7 +27,7 @@ def insert_data(user):
         database = pickle.load(open('database.pkl', 'rb'))
 
     if user.name in database:
-        database[user.name].entry.insert(0, {"datetime": user.datetime, "height": user.height, "weight": user.weight, "bmi": user.bmi()})
+        database[user.name].entry.insert(0, {"datetime": user.datetime, "height": user.height, "weight": user.weight, "bmi": user.bmi, "status": user.status})
     else:
         database[user.name] = user
 
@@ -47,16 +48,20 @@ def retrieve_data(name):
 
 # ________________ Calculate _______________
 class User(object):
-    def __init__(self, name, height, weight, dob, range):
+    def __init__(self, name, height, weight, dob):
         self.name = name
         self.dob = dob
         self.height = float(height)
         self.weight = float(weight)
-        self.range  = str(range)
         self.datetime = datetime.now()
-        self.entry = [{"datetime": self.datetime, "height": self.height, "weight": self.weight, "bmi": self.bmi()}]
+        self.age = self.get_age()
+        self.bmi = self.bmi()
+        self.status = self.bmi_range()
+        self.entry = [{"datetime": self.datetime, "height": self.height, "weight": self.weight, "bmi": self.bmi, "status": self.status}]
         insert_data(self)
-        self.info = """To increase BMI
+        self.info = self.suggestions()
+
+        """To increase BMI
 Diet:
 Eating more frequently. Slowly begin to eat 5 to 6 smaller meals during the day.
 Choosing food with lots of nutrients. Set up a routine to eat and drink things you like and that have a lot of nutrients as well as calories. 
@@ -84,34 +89,97 @@ Weight lifting
 
     def bmi(self):
         return self.weight / (self.height * self.height)
+
+    def get_age(self):
+        if self.dob:
+            return relativedelta(self.datetime.date(), datetime.strptime(self.dob, '%Y-%m-%d')).years
         
     def bmi_range(self):
-	if bmi(self) < 0:
-		print("Invalid BMI")
-		self.range("Invalid")
-    	elif bmi(self) < 18.5:
-		print("Underweight")
-	    	self.range("Underweight")
-    	elif bmi(self) >= 18.5 and bmi(self) < 25.0:
-		print("Healthy Weight")
-	    	self.range("Healthy Weight")
-    	else:
-		print("Overweight")
-	    	self.range("Overweight")
+        bmi_range = ""
+
+        if self.bmi < 0:
+            bmi_range = "Invalid"
+        elif self.bmi < 18.5:
+            bmi_range = "Underweight"
+        elif 18.5 <= self.bmi < 25.0:
+            bmi_range = "Healthy Weight"
+        else:
+            bmi_range = "Overweight"
+
+        return bmi_range
 
     def suggestions(self):
-	if self.range == "Healthy Weight":
-		print("Good job! Keep it up and maintain your BMI.")
-	elif self.age < 60 and self.range == "Underweight":
-		print("a")
-	elif self.age < 60 and self.range == "Overweight":
-		print("b")
-	elif self.age >= 60 and self.range == "Underweight":
-		print("c")
-	elif self.age >= 60 and self.range == "Overweight":
-		print("d")
-	elif self.range == "Invalid":
-		print("Invalid BMI")
+        suggestion = ""
+
+        if self.age:
+            if self.status == "Healthy Weight":
+                suggestion = "Good job! Keep it up and maintain your BMI."
+            elif self.age < 60 and self.status == "Underweight":
+                suggestion = """To increase BMI
+Diet:
+Eating more frequently. Slowly begin to eat 5 to 6 smaller meals during the day.
+Choosing food with lots of nutrients. Set up a routine to eat and drink things you like and that have a lot of nutrients as well as calories. 
+Add extras to your dishes for more calories, such as cheese in casseroles or nut butter on whole-grain toast. You also can add dry milk or liquid milk to foods for extra protein and calories. Some examples are mashed potatoes or soups.
+Try smoothies and shakes. Avoid beverages with few nutrients or calories, such as diet soda.
+Beverages can make you feel full. If that's the case for you, avoid drinking during a meal or before. But make sure you are drinking enough throughout the day.
+
+Exercise:
+45 minutes of running 3 times a week
+Heavy weight lifting
+Calisthenics
+"""
+            elif self.age < 60 and self.status == "Overweight":
+                suggestion = """Diet:
+High protein and low calorie diet should be taken.
+Drink plenty of fluids
+Eat small portions of meals in short intervals.
+Avoid red meat
+Eat plenty of fruits and vegetables.
+
+Exercise:
+75mins jogging 2 times a week
+Weight lifting
+90mins cycling 1 time a week
+"""
+            elif self.age >= 60 and self.status == "Underweight":
+                suggestion = """Diet:
+Eat small meals and snacks regularly throughout the day, rather than attempting to eat three large meals
+Keeping a ready supply of healthy snacks (e.g. dried fruits and unsalted nuts) within easy reach around your home
+Avoiding foods containing high levels of sugar or saturated fats, as these are full of empty calories
+Increasing their intake of full-fat milk or cheese; try adding these ingredients to meals or drinks they already consume regularly
+Take on more high-energy foods that are rich in calories, such as nuts, beans, pulses, porridge, olive oil and pasta
+
+Exercise:
+45 mins of light jogging, 3 times a week
+"""
+            elif self.age >= 60 and self.status == "Overweight":
+                """Diet:
+High protein and low calorie diet should be taken.
+Drink plenty of fluids
+Eat small portions of meals in short intervals.
+Avoid red meat
+Eat plenty of fruits and vegetables.
+
+Exercises:
+Walking up to 45 minutes a day
+Light Yoga under supervision
+Indoor cycling
+Light weight training
+Pilates
+
+Exercises to be avoided include:
+
+Bench-press
+Pull ups
+Squats
+Long distance cycling
+Anything else that requires great body effort or produces stress on joints.
+"""
+            elif self.status == "Invalid":
+                print("Invalid BMI")
+
+            return suggestion
+
 
 # _________________ API _________________-
 app = Flask(__name__)
@@ -121,15 +189,16 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/", methods=['POST', 'GET'])
 def calculate():
     if request.method == 'POST':
-        print(request.form)
         usr = User(request.form['name'], request.form['height'], request.form['weight'], request.form['DOB'])
         return vars(usr)
     else:
         return render_template('main.html')
 
+
 @app.route("/database")
 def database_page():
     return render_template('database.html')
+
 
 @app.route('/user')
 def show_user_profile():
