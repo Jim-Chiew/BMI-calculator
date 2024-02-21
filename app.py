b0846b9690b0358e2a21c44eb0b209741a75aeae
Jim <jimchiew112233@gmail.com>
2024-02-21 09:43:40 +0800

    Updated integration of age calculator, status and suggestions.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, make_response
 import pickle
 from pandas import DataFrame
 import os.path
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
@@ -61,32 +61,6 @@ class User(object):
         insert_data(self)
         self.info = self.suggestions()
 
-        """To increase BMI
-Diet:
-Eating more frequently. Slowly begin to eat 5 to 6 smaller meals during the day.
-Choosing food with lots of nutrients. Set up a routine to eat and drink things you like and that have a lot of nutrients as well as calories. 
-Add extras to your dishes for more calories, such as cheese in casseroles or nut butter on whole-grain toast. You also can add dry milk or liquid milk to foods for extra protein and calories. Some examples are mashed potatoes or soups.
-Try smoothies and shakes. Avoid beverages with few nutrients or calories, such as diet soda.
-Beverages can make you feel full. If that's the case for you, avoid drinking during a meal or before. But make sure you are drinking enough throughout the day.
-
-Exercise:
-45 minutes of running 3 times a week
-Heavy weight lifting
-Calisthenics
-
-To decrease BMI
-Diet:
-High protein and low calorie diet should be taken.
-Drink plenty of fluids
-Eat small portions of meals in short intervals.
-Avoid red meat
-Eat plenty of fruits and vegetables.
-
-Exercise:
-75mins jogging 2 times a week
-Weight lifting
-90mins cycling 1 time a week"""
-
     def bmi(self):
         return self.weight / (self.height * self.height)
 
@@ -95,8 +69,6 @@ Weight lifting
             return relativedelta(self.datetime.date(), datetime.strptime(self.dob, '%Y-%m-%d')).years
         
     def bmi_range(self):
-        bmi_range = ""
-
         if self.bmi < 0:
             bmi_range = "Invalid"
         elif self.bmi < 18.5:
